9c3d26b302debe160d0d7940108a419aceeee495
Wojciech Wichary <wojciechwichary.biz@gmail.com>
2020-08-10 19:45:40 +0200

    base style and structure

---
+++ main.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.hamburger-js');
+const menu = document.querySelector('.menu-js')
+const menuLinks = document.querySelectorAll('.menu-link');
+
+
+const openMenu = () => {
+    console.log('open');
+    hamburger.classList.toggle('hamburger-active');
+    menu.classList.toggle('menu-active');
+
+    if (menu.classList.contains('menu-active')) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = 'visible'
+    }
+
+}
+
+const closeMenu = () => {
+    menu.classList.remove('menu-active')
+}
+
+menuLinks.forEach(el => {
+    el.addEventListener('click', closeMenu)
+})
+
+
+
+
+hamburger.addEventListener('click', openMenu)
